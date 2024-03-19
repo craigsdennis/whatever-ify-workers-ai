b13f2f918f86ce3f24e35a3670dfbe04f680ed39
Craig Dennis <craigsdennis@gmail.com>
2024-03-19 11:36:41 -0700

    Initial commit

---
+++ public/static/app.js
@@ -0,0 +1,181 @@
+const { createApp } = Vue;
+
+createApp({
+  data() {
+    return {
+      hobbies: "",
+      image: null,
+      imageDescription: "",
+      prompt: "",
+      creativeScene: "",
+      creativeCharacter: "",
+      muppetImage: null,
+      isWebCamOn: false,
+    };
+  },
+  methods: {
+    toggleWebCam() {
+      if (!this.isWebCamOn) {
+        this.isWebCamOn = true;
+        navigator.mediaDevices
+          .getUserMedia({ video: true })
+          .then((stream) => {
+            document.getElementById("webcam").srcObject = stream;
+          })
+          .catch((err) => {
+            console.error("Error accessing webcam: ", err);
+          });
+      } else {
+        // Stop the webcam stream
+        const video = document.getElementById("webcam");
+        const stream = video.srcObject;
+        const tracks = stream.getTracks();
+        tracks.forEach((track) => track.stop());
+        video.srcObject = null; // Clear the video source
+        this.isWebCamOn = false;
+      }
+    },
+    capturePhoto() {
+      const video = this.$refs.webcam;
+      const canvas = this.$refs.snapshot;
+      canvas.width = video.videoWidth;
+      canvas.height = video.videoHeight;
+      const context = canvas.getContext("2d");
+      context.drawImage(video, 0, 0);
+      this.image = canvas.toDataURL();
+      canvas.toBlob((blob) => this.updateDescription(blob));
+      this.toggleWebCam();
+    },
+    triggerFileInput() {
+      this.$refs.fileInput.click();
+    },
+    handleFileUpload(event) {
+      const file = event.target.files
+        ? event.target.files[0]
+        : event.dataTransfer.files[0];
+      this.processFile(file);
+    },
+    handleDragOver(event) {
+      // This method is necessary to prevent the default behavior,
+      // allowing us to drop files into our drop area.
+    },
+    handleDrop(event) {
+      const file = event.dataTransfer.files[0];
+      this.processFile(file);
+    },
+    processFile(file) {
+      if (file && file.type.startsWith("image/")) {
+        const reader = new FileReader();
+        reader.onload = (e) => {
+          const img = new Image();
+          img.onload = () => {
+            const canvas = document.createElement("canvas");
+            const maxWidth = 600;
+            const scaleSize = maxWidth / img.width;
+            canvas.width = maxWidth;
+            canvas.height = img.height * scaleSize;
+
+            const ctx = canvas.getContext("2d");
+            ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+
+            canvas.toBlob(
+              (blob) => {
+                this.image = URL.createObjectURL(blob);
+                this.updateDescription(blob);
+              },
+              file.type,
+              1
+            );
+          };
+          img.src = e.target.result;
+        };
+        reader.readAsDataURL(file);
+      } else {
+        alert("Please upload an image file.");
+      }
+    },
+    async handleStreamingResponse(url, body, onvalue) {
+      let whole = "";
+      console.log({ url });
+      console.log({ body });
+      try {
+
+        const response = await fetch(url, {
+          method: "POST",
+          body: JSON.stringify(body),
+        });
+        const reader = response.body
+          .pipeThrough(new TextDecoderStream())
+          .getReader();
+        while (true) {
+          const { value, done } = await reader.read();
+          if (done) {
+            console.log("Stream done", url);
+            return whole;
+          }
+          //this.prompt += value;
+          onvalue(value);
+          whole += value;
+        }
+      } catch(err) {
+        console.error("HI MOM", err);
+      }
+    },
+    async generateCreative() {
+      const creatives = await Promise.all([
+        this.handleStreamingResponse(
+          "/api/creative/scene",
+          { hobbies: this.hobbies },
+          (val) => (this.creativeScene += val)
+        ),
+        this.handleStreamingResponse(
+          "/api/creative/character",
+          { description: this.imageDescription, hobbies: this.hobbies },
+          (val) => (this.creativeCharacter += val)
+        ),
+      ]);
+      console.log(creatives);
+      console.log(this.creativeScene);
+      console.log(this.creativeCharacter);
+      await this.generateStableDiffusionPrompt(
+        this.creativeScene,
+        this.creativeCharacter
+      );
+    },
+    async generateStableDiffusionPrompt(scene, character) {
+      await this.handleStreamingResponse(
+        "/api/prompts/stable-diffusion",
+        { scene, character },
+        (val) => (this.prompt += val)
+      );
+      await this.updateMuppet();
+    },
+    async updateDescription(image) {
+      const formData = new FormData();
+      formData.append("photo", image);
+      const response = await fetch("/api/images/description", {
+        method: "POST",
+        body: formData,
+      });
+      const json = await response.json();
+      this.imageDescription = json.result;
+      this.generateCreative();
+    },
+    async fetchMuppetImage(prompt) {
+      console.log("Fetching image", prompt);
+      const response = await fetch("/api/images/muppet", {
+        method: "POST",
+        body: JSON.stringify({
+          prompt,
+        }),
+      });
+      const blob = await response.blob();
+      return URL.createObjectURL(blob);
+    },
+    async updateMuppet() {
+      const url = await this.fetchMuppetImage(this.prompt);
+      console.log(url);
+      this.muppetImage = url;
+    },
+  },
+}).mount("#app");
---
+++ src/index.tsx
@@ -0,0 +1,227 @@
+import { Ai } from "@cloudflare/ai";
+import { Context, Hono } from "hono";
+import { streamText } from "hono/streaming";
+import { stripIndents } from "common-tags";
+import { EventSourceParserStream } from "eventsource-parser/stream";
+
+import index_html from "../public/index.html?raw";
+
+type Bindings = {
+  AI: Ai;
+};
+
+const app = new Hono<{ Bindings: Bindings }>();
+
+app.get("/", (c) => {
+  return c.html(index_html);
+});
+
+app.post("/api/images/description", async (c) => {
+  // Accept photo
+  type PhotoBodyData = {
+    photo: File;
+  };
+  const data: PhotoBodyData = await c.req.parseBody();
+  const photo = data["photo"];
+  const ai = new Ai(c.env.AI);
+
+  // The puppet maker only has three colors of felt and they are "White", "Brown", or "Black". Choose the color that best matches the person's skin tone/color.
+  const prompt = stripIndents`
+  Describe the person in this photo in great enough detail to provide an artist the ability to recreate them.  
+
+  Ensure you convey the person's distinctive features, their current expression, and overall essence.
+  
+  Include specifics about their face, hairstyle, clothing, and demeanor.
+
+  Do not describe anything else in the photo except the person. 
+  
+  Do not describe where they are located or anything in the background.
+    
+  Provide a detail rich explanation of the person.
+  `;
+  const response = await ai.run("@cf/unum/uform-gen2-qwen-500m", {
+    prompt,
+    image: [...new Uint8Array(await photo.arrayBuffer())],
+  });
+  return c.json({ result: response.description });
+});
+
+async function promptStream(
+  c: Context<{ Bindings: Bindings }>,
+  systemMessage: string,
+  userPrompt: string
+) {
+  const ai = new Ai(c.env.AI);
+  let eventSourceStream;
+  let hasValidResponse = false;
+  let retryCount = 0;
+  while (!hasValidResponse && retryCount < 5) {
+    try {
+      eventSourceStream = (await ai.run(
+        "@hf/thebloke/openhermes-2.5-mistral-7b-awq",
+        {
+          messages: [
+            { role: "system", content: systemMessage },
+            { role: "user", content: userPrompt },
+          ],
+          stream: true,
+        }
+      )) as ReadableStream<any>;
+      hasValidResponse = true;
+    } catch(err) {
+      console.error(err);
+      retryCount++;
+      if (retryCount >= 5) {
+        throw err;
+      } else {
+        console.log(`Retry #${retryCount}...`);
+      }
+    }
+  }
+  if (eventSourceStream === undefined) {
+    throw new Error(`Problem with model.`)
+  }
+  const tokenStream = eventSourceStream
+    .pipeThrough(new TextDecoderStream())
+    .pipeThrough(new EventSourceParserStream());
+  return streamText(c, async (stream) => {
+    for await (const msg of tokenStream) {
+      if (msg.data !== "[DONE]") {
+        const data = JSON.parse(msg.data);
+        stream.write(data.response);
+      }
+    }
+  });
+}
+
+app.post("/api/creative/scene", async (c) => {
+  const json = await c.req.json();
+  const ai = new Ai(c.env.AI);
+  const systemMessage = stripIndents`
+  You are a set designer on Sesame Street.
+
+  The user is going to provide you with a list of hobbies.
+
+  Your task is to design a single fun scene where all of the hobbies could take place.
+
+  Include your lighting ideas and still shot framing.
+
+  This is going to be used for a photo shoot.
+
+  Limit your response to 4 sentences.
+  `;
+  return promptStream(c, systemMessage, json.hobbies);
+});
+
+app.post("/api/creative/character", async (c) => {
+  const json = await c.req.json();
+  const ai = new Ai(c.env.AI);
+  const systemMessage = stripIndents`
+  You are a Muppet creator.
+
+  The user is going to provide a description of a person.
+
+  Your job is to extract only information about the person and their features. 
+  
+  Ignore where the user is located physically, or any other irrelevant information.
+
+  Your final task is to craft a description of a new Muppet that exaggerates their distinctive features.
+
+  Their hobbies are also included.
+
+  Try to create a rich description that will capture their essence and expression in your description.
+
+  Choose a color for your Muppet's Skin Color and include it in your description.
+
+  Limit your character to 4 sentences.
+  `;
+  const userPrompt = stripIndents`
+  Physical Description: ${json.description}
+
+  Hobbies: ${json.hobbies}
+  `
+  return promptStream(c, systemMessage, userPrompt);
+});
+
+app.post("/api/prompts/stable-diffusion", async (c) => {
+  const json = await c.req.json();
+  const ai = new Ai(c.env.AI);
+  // Send to Mistral
+  // If the word man or boy is in the description, assume that is the muppet. Use "((male muppet:1))" in place of man, and if woman or girl use "((female muppet:1))"
+
+  // Include one of "(muppet color yellow)", "(muppet color green)", "(muppet color blue)", "(muppet color pink), or "muppet color purple" based on their description.
+  //     Determine the skin color of the muppet based on what will complement the color the best.
+
+  // Include the skin color of the muppet in the prompt.
+
+  // Do not write prose, but instead capture the important elements of the scene in short blurbs separated by commas.
+    
+  // Put parenthesis around the most important elements. For instance if the character is described as muscular write "strong, (muscular), smile"
+  
+  // End the prompt with something like "Capture the essence of this (Muppet:1.1) with an artistic, vibrant, and lifelike representation. (photo-realistic)"
+
+  const systemMessage = stripIndents`
+    You are a Stable Diffusion Prompt Engineer. 
+
+    The user is going to describe a Scene and a Character.
+
+    Your task is to produce a Stable Diffusion XL Lightning prompt.
+    
+    The photo should be to capture the essence of the Character front and center in the middle of the Scene.
+
+    Create the scene and describe the character using the best practices of Stable Diffusion prompt engineering.
+
+    Make sure to include a nice summary of both the Character and the Scene, so that it can be rendered as close to the user's request as possible.
+
+    Place parentheses around words that are critical to include. For example "(blue polkadot shirt)"
+
+    Ensure that you create the Character, with name if available, and focus on it in the center of the image.
+
+    Try to match their outfit to their description.
+
+    As is best practice for Stable Diffusion, try to not be verbose, separating the most important parts of your descriptions with commas. 
+    
+    Do not make it read like a sentence, build a Stable Diffusion XL Lightning prompt. 
+        
+    Do not use location information from the Character's description, describe them in the center of the scene's description.
+
+    If the user requests specific important information ensure to honor their request.
+
+    Do not include labels like "Scene:" or "Character:" in your prompt. 
+
+    Only return the Stable Diffusion Prompt, no explanations.
+    
+    Do not include a prefix or suffix for the prompt, just return the prompt.
+  `;
+  const userPrompt = stripIndents`
+    Included below is a Sesame Street Scene and a Muppet Character that you should use to create the Stable Diffusion prompt.
+
+    Make a realistic photo of this new Muppet in the center of the scene described below.
+
+    It is super important that the prompt includes the word Muppet.
+    
+    Only refer to muppets, not humans. For instance if description says Man or boy, instead use "male muppet", if it says woman or girl use "female muppet"
+
+    End the prompt with something like "Capture the essence of this (Muppet:1.1) with an artistic, vibrant, and lifelike representation. photo realistic."
+
+    Scene: ${json.scene}
+    
+    Character: ${json.character}
+  `;
+  return promptStream(c, systemMessage, userPrompt);
+});
+
+app.post("/api/images/muppet", async (c) => {
+  const json = await c.req.json();
+  console.log("Creating muppet", json);
+  const ai = new Ai(c.env.AI);
+  // Runs Stable diffusion model
+  const response = await ai.run("@cf/bytedance/stable-diffusion-xl-lightning", {
+    prompt: json.prompt,
+  });
+  return c.body(response, 200, {
+    "Content-Type": "image/x-png",
+  });
+});
+
+export default app;
