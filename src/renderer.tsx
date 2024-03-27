263d3b59beea113325501f1a38bdb154b7b51d47
Craig Dennis <craigsdennis@gmail.com>
2024-03-27 11:45:19 -0700

    Adds footer to home

--- src/renderer.tsx
+++ src/renderer.tsx
@@ -7,7 +7,11 @@ export const renderer = jsxRenderer(({ children, title }) => {
         <link href="/static/style.css" rel="stylesheet" />
         <title>{title}</title>
       </head>
-      <body>{children}</body>
+      <body>{children}
+      <div class="footer">
+        Use of <em>Whateverify</em> is subject to the Cloudflare Website and Online Services <a href="https://www.cloudflare.com/website-terms/">Terms of Use</a> and <a href="https://www.cloudflare.com/privacypolicy/">Privacy Policy</a>
+      </div>
+      </body>
     </html>
   )
 })
