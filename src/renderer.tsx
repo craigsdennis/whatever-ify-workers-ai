0069eb4b022ddeb62f9f77b3aacaea52c563f9e8
Craig Dennis <craigsdennis@gmail.com>
2024-03-27 11:51:05 -0700

    Adds footer

--- src/renderer.tsx
+++ src/renderer.tsx
@@ -9,7 +9,9 @@ export const renderer = jsxRenderer(({ children, title }) => {
       </head>
       <body>{children}
       <div class="footer">
-        Use of <em>Whateverify</em> is subject to the Cloudflare Website and Online Services <a href="https://www.cloudflare.com/website-terms/">Terms of Use</a> and <a href="https://www.cloudflare.com/privacypolicy/">Privacy Policy</a>
+        <p>Built with 🧡 on Cloudflare <a href="https://ai.cloudflare.com">Workers AI</a></p>
+
+        <p>Use of <em>Whateverify</em> is subject to the Cloudflare Website and Online Services <a href="https://www.cloudflare.com/website-terms/">Terms of Use</a> and <a href="https://www.cloudflare.com/privacypolicy/">Privacy Policy</a></p>
       </div>
       </body>
     </html>
